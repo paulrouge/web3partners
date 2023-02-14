dddb68fc805fbcf8a5c17e6c1226e47fa9a9438f
Paul Rouge <paulrouge@outlook.com>
2023-02-14 13:17:03 +0100

    init nextjs typescript tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
+  <Layout title="Web3 Partners">
+    <div className='bg-web3blue'>
+      <h1>Hello Next.js 👋</h1>
+    </div>
   </Layout>
 )
 
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        web3blue: '#0000FF',
+        web3mint: '#80FFCB',
+        web3pink: '#F400E9',
+      },
+    },
+  },
+  plugins: [],
+}
