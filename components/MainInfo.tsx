10fb381720228d85efb946be3a0bf9f43ae043ae
Paul Rouge <paulrouge@outlook.com>
2023-02-15 16:18:06 +0100

    edited some text

--- components/MainInfo.tsx
+++ components/MainInfo.tsx
@@ -11,10 +11,10 @@ const MainInfo = () => {
         px-8 py-8 text-xl bg-web3pink
         md:w-2/3 md:rounded md:mt-20 md:shadow
         '>
-            Web 3 Partners is a global community of Web 3 enthusiasts, developers, and investors. We are building a decentralized ecosystem of Web 3 projects and companies.
+            Web3 Partners is a space for collaboration, education and support for those who are interested in developing projects and communities on web3.
             <br/><br/>
             On the 23rd of February we are meeting up in Delft for the first time. There will be a panel discussion and 
-            speakers after which we will have a networking session. We will also be announcing the first Web 3 Partners
+            speakers after which we will have a networking session.
         </div>
 
     </div>
