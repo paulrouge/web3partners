105cf27f5e2301b833be502fffdfeacfb0d1f775
David <98425402+DavidFBD@users.noreply.github.com>
2023-03-03 15:14:07 +0100

    Added form and footer edits

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <div className='w-full px-4 py-1 text-right bg-web3blue text-web3mint font-web3'>
+    <div className='w-full px-4 py-1 text-sm text-center bg-web3blue text-web3mint font-web3'>
       Web3 Partners: the Netherlands
     </div>
   )
